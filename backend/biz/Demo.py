9cdda745fe33451f864c7495b041c684e23a15a6
LuoRan <rowland.lancer@gmail.com>
2016-03-24 19:33:18 +0800

    Update Demo.py

--- backend/biz/Demo.py
+++ backend/biz/Demo.py
@@ -6,6 +6,7 @@ class Hello(object):
     @Router.routine(url='api/hello/(\w+)', method=Router.POST | Router.GET, timeout=5)
     def test(self, params, name):
         '''params是表单数据，name为正则表达式捕获的参数，
-        如果有多个正则参数会依次注入到函数的参数列表中'''
+            如果有多个正则参数会依次注入到函数的参数列表中
+        '''
         age = params.get('age', '13')
         return 'hello! My name is ' + name + ', I\'m ' + age + ' years old'
